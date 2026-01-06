5a8100290a79531a8e9b339f9ca13d77e882aa5c
Jae <lijae3552@gmail.com>
2026-01-06 21:41:00 +0900

    ini: initial repo setting

---
+++ src/test/java/com/tril/billing_platform/BillingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tril.billing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
