0455fe6a97df50c5d2031b25455d87aab11bf694
marulog <gr214777@naver.com>
2026-01-15 23:04:36 +0900

    feat: Billing VO 경로 수정

--- contexts/billing/src/main/java/com/tr1l/billing/domain/model/vo/EncryptedEmail.java
+++ contexts/billing/src/main/java/com/tr1l/billing/domain/model/vo/EncryptedEmail.java
@@ -1,4 +1,4 @@
-package com.tr1l.billing.domain.vo;
+package com.tr1l.billing.domain.model.vo;
 
 import com.tr1l.billing.domain.exception.BillingDomainException;
 import com.tr1l.billing.error.BillingErrorCode;
