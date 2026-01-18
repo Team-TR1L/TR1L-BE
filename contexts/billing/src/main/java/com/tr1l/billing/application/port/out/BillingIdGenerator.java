00b75b5a63a51cf9d6940f750bf5d153c035f89a
marulog <gr214777@naver.com>
2026-01-19 06:08:45 +0900

    feat: writer-> 완성된 청구 데이터 적재

--- contexts/billing/src/main/java/com/tr1l/billing/application/port/out/BillingIdGenerator.java
+++ contexts/billing/src/main/java/com/tr1l/billing/application/port/out/BillingIdGenerator.java
@@ -3,5 +3,6 @@ package com.tr1l.billing.application.port.out;
 import com.tr1l.billing.domain.model.vo.BillingId;
 
 public interface BillingIdGenerator {
-    BillingId nextId();
+    //  workId 기반으로 BillingId 생성
+    BillingId generateForWork(String workId);
 }
