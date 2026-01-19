d00883e3827a114c9da8cd79674b2d328dc08f8b
cylin0201 <cylin0201@naver.com>
2026-01-19 19:10:47 +0900

    [TR1L-65] refactor: SUCCED 오타 수정

--- contexts/dispatch/src/main/java/com/tr1l/delivery/infra/config/AsyncConfig.java
+++ contexts/dispatch/src/main/java/com/tr1l/delivery/infra/config/AsyncConfig.java
@@ -14,7 +14,7 @@ public class AsyncConfig {
     public Executor notificationExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(50);   // 스레드 50개 유지
-        executor.setMaxPoolSize(200);   // 최대 200개까지 확장
+        executor.setMaxPoolSize(80);   // 최대 200개까지 확장
         executor.setQueueCapacity(500);
         executor.initialize();
         return executor;
--- contexts/dispatch/src/main/java/com/tr1l/dispatch/domain/model/enums/MessageStatus.java
+++ contexts/dispatch/src/main/java/com/tr1l/dispatch/domain/model/enums/MessageStatus.java
@@ -4,6 +4,6 @@ public enum MessageStatus {
     READY,
     PROCESSING,
     SENT,
-    SUCCEEDED,
+    SUCCEED,
     FAILED
 }
