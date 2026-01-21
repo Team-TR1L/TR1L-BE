dc2e11746621f1f07715baf7385b45bcc71e5534
marulog <gr214777@naver.com>
2026-01-22 02:22:46 +0900

    chore: comment 정리

--- contexts/billing/src/main/java/com/tr1l/billing/application/dto/BillingTargetRow.java
+++ contexts/billing/src/main/java/com/tr1l/billing/application/dto/BillingTargetRow.java
@@ -26,14 +26,10 @@ public record BillingTargetRow(
         boolean soldierEligible, // 군인 유무
 
         boolean welfareEligible, //복지 유무
-        String welfareCode,  // 복지 유형 "WLF-01, 02, 03"
-        String welfareName, // 표시용 "장애인 할인", "국가유공자할인", "기초생활수급자할인"
+        String welfareCode,  // 복지 유형 "WLF-01, 02, 03 04 05"
+        String welfareName, // 표시용 "일반인", "장애인 할인", "국가유공자할인", "기초생활수급자할인" "기초연금수급자"
         double welfareRate, // 복지 할인  0.3
         Long welfareCapAmount, // 복지 상한선 -> 장애인은 0, 나머지 12000 등
-
-//        boolean bundleEligible, // 결합 유무
-//        Long bundleTotalDiscountAmount, // 유저의 결합 할인액
-//        String bundleName, // 표시용 결합이름 -> "가족무한사랑"
 
         String optionsJsonb// 원문 JSONB [{ "name":"디즈니+", "monthlyPrice":9405 }, { "name":"티빙",   "monthlyPrice":4950 }]
 ) {}
