2e89966c2459139e6163aadf7533e4412d3f7b72
최승언 <chygold77@naver.com>
2026-01-21 13:24:17 +0900

    [TR1L-70] fix: Scan 범위 수정

--- apps/api-server/src/main/java/com/tr1l/apiserver/ApiServerApplication.java
+++ apps/api-server/src/main/java/com/tr1l/apiserver/ApiServerApplication.java
@@ -2,8 +2,12 @@ package com.tr1l.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.tr1l.dispatch","com.tr1l.util"})
+@SpringBootApplication(scanBasePackages = {"com.tr1l.util"})
+@EnableJpaRepositories(basePackages = "com.tr1l")
+@EntityScan(basePackages = "com.tr1l")
 public class ApiServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class);
