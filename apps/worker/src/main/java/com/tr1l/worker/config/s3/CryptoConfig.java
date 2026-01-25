f50862c270eac0b82bec32a6b6a1228f5a8eb42a
marulog <gr214777@naver.com>
2026-01-26 03:16:28 +0900

    feat: 암호화 환경변수 변경

--- apps/worker/src/main/java/com/tr1l/worker/config/s3/CryptoConfig.java
+++ apps/worker/src/main/java/com/tr1l/worker/config/s3/CryptoConfig.java
@@ -11,9 +11,9 @@ public class CryptoConfig {
 
     @Bean
     public EncryptionTool encryptionTool(
-            @Value("${crypto.secret-key}") String secretKey,
-            @Value("${crypto.algorithm}") String algorithm,
-            @Value("${crypto.transformation}") String transformation
+            @Value("${secret-key}") String secretKey,
+            @Value("${algorithm}") String algorithm,
+            @Value("${transformation}") String transformation
     ) {
         validateKeyLength(secretKey);
         return new EncryptionTool(secretKey, algorithm, transformation);
@@ -21,9 +21,9 @@ public class CryptoConfig {
 
     @Bean
     public DecryptionTool decryptionTool(
-            @Value("${crypto.secret-key}") String secretKey,
-            @Value("${crypto.algorithm}") String algorithm,
-            @Value("${crypto.transformation}") String transformation
+            @Value("${secret-key}") String secretKey,
+            @Value("${algorithm}") String algorithm,
+            @Value("${transformation}") String transformation
     ) {
         validateKeyLength(secretKey);
         return new DecryptionTool(secretKey, algorithm, transformation);
